778389ce22909fc849313a1fa3d845099de31261
higoreduardodocs <higoreduardodocs@gmail.com>
2024-04-25 09:38:31 -0400

    :sparkles: Feat: create register user

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
 
 
 def cadastro(request):
@@ -7,6 +10,39 @@ def cadastro(request):
   # return HttpResponse('Hello world')
   if request.method == 'GET':
     return render(request, 'cadastro.html')
+  elif request.method == 'POST':
+    username = request.POST.get('username')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    confirmar_senha = request.POST.get('confirmar_senha')
+
+    users = User.objects.filter(username=username)
+
+    if users.exists():
+      messages.add_message(request, constants.ERROR, 'Usuário já cadastrado')
+      return redirect('/usuarios/cadastro/')
+    
+    users = User.objects.filter(email=email)
+
+    if users.exists():
+      messages.add_message(request, constants.ERROR, 'Email já cadastrado')
+      return redirect('/usuarios/cadastro/')
+
+    if senha != confirmar_senha:
+      messages.add_message(request, constants.ERROR, 'Senha e confirmar senha devem ser iguais')
+      return redirect('/usuarios/cadastro/')
+    
+    if len(senha) < 6:
+      messages.add_message(request, constants.ERROR, 'Senha deve ter mais de 6 caracteres')
+      return redirect('/usuarios/cadastro/')
+    
+    User.objects.create_user(
+      username=username,
+      email=email,
+      password=senha
+    )
+    messages.add_message(request, constants.SUCCESS, 'Cadastro realizado com sucesso')
+    return redirect('/usuarios/login/')
 
 def login(request):
   if request.method == 'GET':
