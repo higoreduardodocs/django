83e486e71d7222e179f725f838910dbb34d66ca2
higoreduardodocs <higoreduardodocs@gmail.com>
2024-04-29 16:58:08 -0400

    :recycle: Refactor: change pacientes routes

--- pacientes/urls.py
+++ pacientes/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
   path('home/', views.home, name='home'),
-  path('escolher-horario/<int:id_dados_medico>', views.escolher_horario, name='escolher-horario'),
+  path('escolher-horario/<int:id_dados_medico>', views.escolhe_horario, name='escolher-horario'),
   path('agendar-horario/<int:id_agenda_aberta>', views.agendar_horario, name='agendar-horario'),
   path('minhas-consultas/', views.minhas_consultas, name='minhas-consultas'),
   path('consulta/<int:id_consulta>', views.consulta, name='consulta'),
+  path('cancelar-consulta/<int:id_consulta>', views.cancelar_consulta, name='cancelar-consulta'),
 ]
--- pacientes/views.py
+++ pacientes/views.py
@@ -1,23 +1,30 @@
 from django.shortcuts import render, redirect
-from datetime import datetime
-from django.contrib import messages
-from django.contrib.messages import constants
 from medicos.models import DadosMedico, Especialidade, DatasAbertas, is_medico
-from .models import Consulta
+from datetime import datetime
+from .models import Consulta, Documento
+from django.contrib.messages import constants
+from django.contrib import messages
 
 
 def home(request):
   if request.method == 'GET':
+    medico_filtrar = request.GET.get('medico')
+    especialidades_filtrar = request.GET.getlist('especialidades')
     medicos = DadosMedico.objects.all()
+
+    if medico_filtrar:
+      medicos = medicos.filter(nome__icontains=medico_filtrar)
+    if especialidades_filtrar:
+      medicos = medicos.filter(especialidade_id__in=especialidades_filtrar)
+
     especialidades = Especialidade.objects.all()
 
     return render(request, 'home.html', {'medicos': medicos, 'especialidades': especialidades, 'is_medico': is_medico(request.user)})
 
-def escolher_horario(request, id_dados_medico):
+def escolhe_horario(request, id_dados_medico):
   if request.method == 'GET':
     medico = DadosMedico.objects.get(id=id_dados_medico)
     datas_abertas = DatasAbertas.objects.filter(user=medico.user).filter(data__gt=datetime.now()).filter(agendado=False)
-    print(datas_abertas)
 
     return render(request, 'escolher_horario.html', {'medico': medico, 'datas_abertas': datas_abertas, 'is_medico': is_medico(request.user)})
 
@@ -47,7 +54,7 @@ def minhas_consultas(request):
         consultas.filter(data_agendada__data__gte=data_formatada)
 
     return render(request, 'minhas_consultas.html', {'consultas': consultas, 'is_medico': is_medico(request.user)})
-    
+  
 def consulta(request, id_consulta):
   if request.method == 'GET':
     consulta = Consulta.objects.get(id=id_consulta)
@@ -56,6 +63,17 @@ def consulta(request, id_consulta):
       return redirect('/paciente/minhas-consultas/')
     
     dado_medico = DadosMedico.objects.get(user=consulta.data_agendada.user)
-    # documentos = Documento.objects.filter(consulta=consulta)
+    documentos = Documento.objects.filter(consulta=consulta)
 
-    return render(request, 'consulta.html', {'consulta': consulta, 'dado_medico': dado_medico, 'documentos': [], 'is_medico': is_medico(request.user)})
+    return render(request, 'consulta.html', {'consulta': consulta, 'dado_medico': dado_medico, 'documentos': documentos, 'is_medico': is_medico(request.user)})
+  
+def cancelar_consulta(request, id_consulta):
+  consulta = Consulta.objects.get(id=id_consulta)
+  if consulta.paciente != request.user:
+      messages.add_message(request, constants.ERROR, 'Está consulta não é sua')
+      return redirect('/paciente/minhas-consultas/')
+  
+  consulta.status = 'C'
+  consulta.save()
+  messages.add_message(request, constants.SUCCESS, 'Consulta cancelar com sucesso')
+  return redirect(f'/pacientes/consulta/{id_consulta}')
